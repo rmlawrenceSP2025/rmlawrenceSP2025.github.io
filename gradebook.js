c00dd7a7928ba7b5e7a74b9fc457f2c4531ef997
rmlawrenceSP2025 <rmlawrence1@my.waketech.edu>
2025-04-08 22:59:16 -0400

    Gradebook
    Gradebook .html and .js committed

---
+++ gradebook.js
@@ -0,0 +1,17 @@
+// TODO: Fetch data from the postgreSQL database (to be implemented later)
+function fetchGradeData() {
+    // This function will query the PostgreSQL database and return grade data
+    console.log("Fetching grade data...");
+}
+
+// TODO: populate the table with grade data
+function populateGradebook(data) {
+    // This function will take the fetched data and populate the table
+    console.log("Populating gradebook with data:", data);
+}
+
+// TODO REMOVE THIS
+// Call the stubs to demonstrate the workflow
+const gradeData = fetchGradeData();
+populateGradebook(gradeData);
+// END REMOVE
